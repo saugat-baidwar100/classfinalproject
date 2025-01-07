7952ca14f613b4370607ac5600340de4a370d707
Shahil-Ahamad-dev <shahilahamadskillprompt@gmail.com>
2025-01-07 16:47:33 +0545

    Setup of Swagger UI

--- apps/lms-backend/src/utils/swagger.ts
+++ apps/lms-backend/src/utils/swagger.ts
@@ -1,9 +1,24 @@
-
+import { chapterContract } from '@skillprompt-lms/libs/api-contract/modules/chapter';
+import { contentContract } from '@skillprompt-lms/libs/api-contract/modules/content';
 import { courseContract } from '@skillprompt-lms/libs/api-contract/modules/courses';
+import { questionContract } from '@skillprompt-lms/libs/api-contract/modules/question';
+import { quizContract } from '@skillprompt-lms/libs/api-contract/modules/quiz';
+import { reviewContract } from '@skillprompt-lms/libs/api-contract/modules/review';
+import { taskProgressContract } from '@skillprompt-lms/libs/api-contract/modules/track-progress';
 import { generateOpenApi } from '@ts-rest/open-api';
 
-// we need to use the main contract to generate the OpenAPI spec for all routes
-export const openApiDocument = generateOpenApi(courseContract, {
+// we need to use the  contract to generate the OpenAPI spec for all routes
+const Contract = {
+  ...chapterContract,
+  ...contentContract,
+  ...courseContract,
+  ...questionContract,
+  ...quizContract,
+  ...reviewContract,
+  ...taskProgressContract,
+};
+
+export const openApiDocument = generateOpenApi(Contract, {
   info: {
     title: 'Courses API',
     version: '1.0.0',
